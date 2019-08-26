1e8cdbb4e783388abcd18f1454d04e2184491831
Milo Goodfellow <milogoodfellowworkemail@gmail.com>
2019-08-26 00:16:42 -0400

    Completed basic lexer
    Commit the most basic and unoptimized version of the Lexer that doesn't throw errors and returns the proper output.

--- Lexer/Lexer.py
+++ Lexer/Lexer.py
@@ -6,11 +6,9 @@ def input_gherkin(input):
     #Clean the feature list of unnecessary spaces and excess list positions.
     for i in range(0,len(feature_list)):
         if feature_list[i].startswith(" "):
-            print(feature_list[i])
             feature_list[i] = feature_list[i][1:len(feature_list[i])]
 
         if feature_list[i].endswith(" "):
-            print(feature_list[i])
             feature_list[i] = feature_list[i][0:-1]
 
     del feature_list[0]
@@ -26,7 +24,6 @@ def input_gherkin(input):
             if temp_scenario_list[x].endswith(" "):
                 temp_scenario_list[x] = temp_scenario_list[x][0:-1]
         feature_scenario_dict[i] = temp_scenario_list
-    print(feature_scenario_dict)
     #Iterate over the scenario dictionary. Divide each scenario into its component parts.
     #Given:
     #And:
@@ -35,35 +32,28 @@ def input_gherkin(input):
     #And:
     #Then:
     #Add all of these strings to a new dictionary. The key being which scenario it is in, the item being a list of all of the above in string form.
-    for key, item in feature_scenario_dict:
+    for key, item in feature_scenario_dict.items():
+        component_parts = []
+        scenario_li = []
         for i in item:
-            component_parts = item.split("\n")
-            for x in range(0, len(component_parts)):
-                if component_parts[x].startswith(" "):
-                    component_parts[x] = component_parts[x][1:len(component_parts[x])]
-                if component_parts[x].endswith(" "):
-                    component_parts[x] = component_parts[x][0:-1]
+            component_parts = i.splitlines()
+            index = 0
+            while index < len(component_parts):
+                for z, y in enumerate(component_parts[index]):
+                    if y.isalpha():
+                        component_parts[index] = component_parts[index][z:]
+                        break
 
-        feature_scenario_dict[key] = component_parts
+                if component_parts[index].isspace() or component_parts[index] == None:
+                    del component_parts[index]
+
+                index += 1
+
+
+
+            scenario_li.append(component_parts)
+        feature_scenario_dict[key] = scenario_li
+
 
     #Once every single scenario has a respective list return the dictionary.
     return feature_scenario_dict
-
-user_in = '''
-Feature: Some terse yet descriptive text of what is desired
-  In order to realize a named business value
-  As an explicit system actor
-  I want to gain some beneficial outcome which furthers the goal
-
-  Scenario: Some determinable business situation
-    Given some precondition
-      And some other precondition
-     When some action by the actor
-      And some other action
-      And yet another action
-     Then some testable outcome is achieved
-      And something else we can check happens too
-
-  Scenario: A different situation
-'''
-print(input_gherkin(user_in))
